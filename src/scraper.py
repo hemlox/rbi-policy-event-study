e3e118a4fd92a2191e52311de0a01e862024c0cd
Darshil T. <drshiltrivedi@gmail.com>
2026-03-27 13:22:15 +0530

    Scraper completed

--- src/scraper.py
+++ src/scraper.py
@@ -1,25 +1,25 @@
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 from selenium.webdriver.common.by import By
-from datetime import date
+from datetime import datetime
 from selenium.common.exceptions import TimeoutException
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions
 from selenium.webdriver.firefox.options import Options
+from datetime import datetime
 import requests
 import sys
 import os
-options = Options()
-options.add_argument("--headless")
-# initially experimenting with just 2026 and only mc
-os.makedirs("artifacts", exist_ok=True)
-year1 = date.today().year
-year =year1
-y = 0
-while year > 2016:#extracting MCP statements 2016-present and gov statements 2021-present
+
+def initialise_driver():
+    options = Options()
+    options.add_argument("--headless")
     driver = webdriver.Firefox(options=options)#headless selenium more efficient
     driver.get("https://rbi.org.in/Scripts/BS_PressreleaseDisplay.aspx")
-    year = year1 - y
+    os.makedirs("artifacts", exist_ok=True)
+    return driver
+
+def navigate_to_year(driver, year):
     id ="btn"+str(year) #year
     id1 = str(year)+"0" #all months
     #navigating to the page for that specific year-->architecture was changed in 2016
@@ -46,6 +46,7 @@ while year > 2016:#extracting MCP statements 2016-present and gov statements 202
             driver.quit()
             sys.exit()
 
+def extract_and_download(driver,year):
     try:
         WebDriverWait(driver,10).until(expected_conditions.presence_of_all_elements_located((By.XPATH, "//a[img[(contains(@alt, 'Monetary Policy') and contains(@alt, 'Resolution')) or (contains(@alt, \"Governor’s Statement\"))]]")))
         #extracts MPC present-2016 and gov staterments from present-2021
@@ -62,6 +63,8 @@ while year > 2016:#extracting MCP statements 2016-present and gov statements 202
     link = []
 
     for file, dates, img in zip(file_names, file_dates, pdf_imgs):#storing all file paths using file name
+        date_obj = datetime.strptime(dates,"%b %d, %Y" )
+        dates = date_obj.strftime("%d-%m-%Y")
         url =  (img.get_attribute("href"))#grabbing href tag for url now need to use url to download the pdf
         link.append(url)
         if "Governor’s" in file:
@@ -76,4 +79,19 @@ while year > 2016:#extracting MCP statements 2016-present and gov statements 202
     print(f"{year}'s Links:", end=" ")
     print(link)
     driver.quit()
-    y = y +1
+
+def main():
+    year = datetime.today().year
+    while(year>2015):
+        driver = initialise_driver()
+        navigate_to_year(driver, year)
+        extract_and_download(driver, year)
+        year = year-1
+
+if __name__ == "__main__":
+    sys.exit(main())
+
+
+
+
+
